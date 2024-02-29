a7a92e8885d64b671a19ccff924f8d883bf69560
ajtritt <ajtritt@lbl.gov>
2024-02-29 14:51:45 -0800

    Update README and usage statement

--- README.md
+++ README.md
@@ -18,12 +18,19 @@ pip install .
 
 # Commands
 
+## Segmentation
+
 - `lmcontrol crop` - Crop light microscopy images to bounding box around single 
                      cell.
 - `lmcontrol prep-viz` - Prepare package for running interactive visualizer. 
                          See `emb-viz` command for more details.
 - `lmcontrol emb-viz` - Launch interactive visualizer for exploring segmented and 
                         cropped images.
+
+## Self-supervised Learning
+
+- `lmcontrol train-byol` - Train a [BYOL](https://arxiv.org/abs/2006.07733) model
+- `lmcontrol infer-byol` - Create embeddings with a trained [BYOL](https://arxiv.org/abs/2006.07733) model
 
 ## notebooks
 
--- src/lmcontrol/__init__.py
+++ src/lmcontrol/__init__.py
@@ -19,8 +19,10 @@ def main():
             'crop': Command('segment.main', 'Crop light microscopy images to bounding box around single cell'),
             'prep-viz': Command('viz.make_package.main', 'Prepare package for visualizing image embeddings'),
             'emb-viz': Command('viz.interactive_viz.main', 'Interactive viewer of image embeddings'),
+        },
+        'Self-supervised Learning': {
             'train-byol': Command('nn.run_byol.main', 'Train BYOL model'),
-            'infer-byol': Command('nn.run_byol.predict', 'Run inference BYOL backbone'),
+            'infer-byol': Command('nn.run_byol.predict', 'Run with inference BYOL backbone'),
         },
     }
     import sys
