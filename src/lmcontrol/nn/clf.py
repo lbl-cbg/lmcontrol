8b0b180266ac0f0bb63ef7499d731701ce94b0a8
NiranjanChaudhari0929 <f20200929@pilani.bits-pilani.ac.in>
2024-07-29 17:16:58 -0700

    Adding ability to filter by label

--- src/lmcontrol/nn/clf.py
+++ src/lmcontrol/nn/clf.py
@@ -1,166 +1,232 @@
-
 import argparse
-import copy
 import glob
-
 import lightning as L
-from lightning.pytorch.loggers import CSVLogger
-from lightning.pytorch.callbacks.early_stopping import EarlyStopping
-import numpy as np
-
 import torch
-from torch.utils.data import DataLoader,Dataset
+from torch.utils.data import DataLoader
 import torchvision
 from torch import nn
-
-from lightly.loss import NegativeCosineSimilarity
-from lightly.models.modules import BYOLPredictionHead, BYOLProjectionHead
-from lightly.models.utils import deactivate_requires_grad, update_momentum
-from lightly.transforms.byol_transform import BYOLTransform
-from lightly.utils.scheduler import cosine_schedule
-
+import torch
+from torch.utils.data import DataLoader
+import glob
+import numpy as np
+import argparse
+import lightning as L
 from ..utils import get_logger
-from .dataset import get_lightly_dataset, get_transforms as _get_transforms
+from ..utils import get_logger
+from .dataset import LMDataset, get_transforms as _get_transforms
+import wandb
+from lightning.pytorch.loggers import WandbLogger
+from sklearn.metrics import accuracy_score, precision_score, recall_score, confusion_matrix
+from lightning.pytorch.callbacks import EarlyStopping
+# from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+
 
 
 class ResNet(L.LightningModule):
 
-    # Note: The model and training settings do not follow the reference settings
-    # from the paper. The settings are chosen such that the example can easily be
-    # run on a small dataset with a single GPU.
-
     val_metric = "validation_ncs"
     train_metric = "train_ncs"
 
-    def __init__(self, num_classes=10):
+    def __init__(self, num_classes):
         super().__init__()
-        # Load the pretrained ResNet18 model
-        self.backbone = torchvision.models.resnet18(pretrained=True)
-        n_features = 512  # Number of features for ResNet18
+        self.backbone = torchvision.models.resnet18(pretrained=False)
+        n_features = 512 
         self.backbone.fc = nn.Linear(n_features, num_classes)
         self.criterion = nn.CrossEntropyLoss()
+        self.save_hyperparameters() #this is an important part, we didnt add it before. STUDY WHY THIS MATTERED 
 
     def forward(self, x):
-        return self.backbone(x)
-    
+        return self.backbone(x) 
 
-    def training_step(self, batch, batch_idx):
+    # def training_step(self, batch, batch_idx): 
+    #     images, labels = batch
+    #     outputs = self.forward(images)
+    #     loss = self.criterion(outputs, labels[:, 0])
+    #     self.log('train_loss', loss, batch_size=images.size(0))
+    #     return loss
+      
+
+    def training_step(self, batch, batch_idx): 
         images, labels = batch
         outputs = self.forward(images)
-        loss = self.criterion(outputs, labels)
+        loss = self.criterion(outputs, labels[:, 0])
+        
+        # Logging accuracy
+        preds = torch.argmax(outputs, dim=1)
+        acc = accuracy_score(labels[:, 0].cpu().numpy(), preds.cpu().numpy())
+        self.log('train_accuracy', acc, on_step=False, on_epoch=True, prog_bar=True)
+        
         self.log('train_loss', loss, batch_size=images.size(0))
-        return loss
+        return loss    
+
+    # def validation_step(self, batch, batch_idx):
+    #     images, labels = batch
+    #     outputs = self.forward(images)
+    #     loss = self.criterion(outputs, labels[:, 0])
+    #     self.log('val_loss', loss, batch_size=images.size(0))
+    #     return loss 
 
     def validation_step(self, batch, batch_idx):
         images, labels = batch
         outputs = self.forward(images)
-        loss = self.criterion(outputs, labels)
+        loss = self.criterion(outputs, labels[:, 0])
+
+        preds = torch.argmax(outputs, dim=1)
+        acc = accuracy_score(labels[:, 0].cpu().numpy(), preds.cpu().numpy())
+        self.log('val_accuracy', acc, on_step=False, on_epoch=True, prog_bar=True)  
+
         self.log('val_loss', loss, batch_size=images.size(0))
-        return loss
-
-
-    def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), lr=0.06)
+        return loss     
     
-    #add scheduler too
-
-def get_transform(transform1=None):
-    """Get BYOL transform
-
-    By default, the two transforms are:
-        transform1: rotate, crop, hflip, vflip, float, rgb
-        transform2: blur, rotate, crop, hflip, vflip, float, noise, rgb
-
-    For more details on these transforms, see lmcontrol.nn.dataset.get_transform
-    """
-    if transform1 is None:
-        transform1 = _get_transforms('float', 'norm', 'rotate', 'crop', 'hflip', 'vflip', 'rgb')
-  
-
-    transform = BYOLTransform(
-        view_1_transform=transform1,
         
-    )
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+        return [optimizer], [scheduler]    
+    
+    def predict_step(self, batch, batch_idx, dataloader_idx=0):  #Added this part, read the pytorch documentation to understand how its used 
+        x = batch[0]
+        return self.forward(x).flatten(start_dim=1)    
+
+
+def get_transform():
+    transform = _get_transforms('float', 'norm', 'rgb')
     return transform
 
 
-def get_npzs(timepoints, hts):
-    ret = list()
-    for tp in timepoints:
-        for ht in hts:
-            ret.extend(glob.glob(f"S{tp}/*HT{ht}/*.npz"))
-    return ret
-
-class MyDataset(Dataset):
-    def __init__(self, file_list, transform=None):
-        self.file_list = file_list
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.file_list)
-
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        img_name = self.file_list[idx]
-        label = self._get_label(img_name)
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
-
-    def _get_label(self, file_path): #finds the label associated 
-
-        label = file_path.split('_')[-2]  
-        return label
-
-
-def get_npzs(timepoints, hts):
-    ret = []
-    for tp in timepoints:
-        for ht in hts:
-            ret.extend(glob.glob(f"S{tp}_H{ht}_*/**/*.npz", recursive=True))
-    return ret
 
 def train(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument("experiment", type=str, help="the experiment name")
-    parser.add_argument("--training", type=str, required=True, help="directory containing training data")
-    parser.add_argument("--validation", type=str, required=True, help="directory containing validation data")
-    parser.add_argument("--testing", type=str, required=True, help="directory containing testing data")
+    parser.add_argument('labels', type=str, nargs='+', help="the label to train with")
+    parser.add_argument("--training", type=str, nargs='+', required=True, help="directories containing training data")
+    parser.add_argument("--validation", type=str, nargs='+', required=True, help="directories containing validation data")
+    parser.add_argument("-c","--checkpoint", type=str, help="path to the model checkpoint file to use for inference")    
     parser.add_argument("-e", "--epochs", type=int, help="the number of epochs to run for", default=10)
     parser.add_argument("-d", "--debug", action='store_true', help="run with a small dataset", default=False)
+    parser.add_argument("-o", "--outdir", type=str, help="the directory to save output to", default='.')
 
     args = parser.parse_args(argv)
 
-    logger = CSVLogger("logs", name=args.experiment)
+    logger = get_logger('info')
 
-    transform = get_transform()
+    transform = get_transform()  
 
-    train_files = get_npzs(args.training)
-    val_files = get_npzs(args.validation)
-    test_files = get_npzs(args.testing)
+    train_files = args.training
+    val_files = args.validation
+    
 
-    train_dataset = MyDataset(train_files, transform=transform)
-    val_dataset = MyDataset(val_files, transform=transform)
-    test_dataset = MyDataset(test_files, transform=transform)
+    logger.info(f"Loading training data: {len(train_files)} files")
+    train_dataset = LMDataset(train_files, transform=transform, logger=logger, return_labels=True, label_types=args.labels)
+    for i in range(train_dataset.labels.shape[1]):
+        logger.info(train_dataset.label_types[i] + " - " + str(torch.unique(train_dataset.labels[:, i])) + str(train_dataset.label_classes))
+    
 
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
-    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=4)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
 
-    model = ResNet()
+    
+    logger.info(f"Loading validation data: {len(val_files)} files")
+    val_dataset = LMDataset(val_files, transform=transform, logger=logger, return_labels=True, label_types=args.labels)
+    for i in range(val_dataset.labels.shape[1]):
+        logger.info(val_dataset.label_types[i] + " - " + str(torch.unique(val_dataset.labels[:, i])) + str(val_dataset.label_classes))
 
-    trainer = L.Trainer(max_epochs=args.epochs, logger=logger)
+    num_workers = 0 if args.debug else 4
+
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, drop_last=True, num_workers=num_workers)
+    val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, drop_last=True, num_workers=num_workers)
+
+    model = ResNet(num_classes=len(train_dataset.label_classes[0]))   # this is a quick fix, it will not work if we specify more than one label
+    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
+    
+    wandb.init(project="SX_HTY_Run1")
+    wandb_logger = WandbLogger(project='your_project_name', log_model=True)
+    # trainer = L.Trainer(max_epochs=args.epochs, devices=1, logger=wandb_logger, accelerator=accelerator)
+    # trainer.fit(model, train_loader, val_loader)
+
+    # EarlyStopping callback
+    early_stopping = EarlyStopping(
+        monitor="val_accuracy",  
+        min_delta=0.000001,      
+        patience=10,         
+        verbose=False,      
+        mode="max"          
+    )
+
+
+    trainer = L.Trainer(
+        max_epochs=10,                       ##CHANGE THIS TO 100
+        devices=1,
+        accelerator=accelerator,
+        #callbacks=[early_stopping],  #UNCOMMENT WHEN DONE
+        logger=wandb_logger  
+    )
+    
     trainer.fit(model, train_loader, val_loader)
 
-    # After training, call the test function
-    test(trainer, model, test_loader)
 
-def test(trainer, model, test_loader):
-    trainer.test(model, test_loader)
+    if args.checkpoint:
+        trainer.save_checkpoint(args.checkpoint)
+        logger.info(f"Model saved to {args.checkpoint}")
+
+
+def predict(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('labels', type=str, nargs='+', help="the label to predict with")
+    parser.add_argument("--prediction", type=str, nargs='+', required=True, help="directories containing prediction data")
+    parser.add_argument("-c","--checkpoint", type=str, help="path to the model checkpoint file to use for inference")      
+    parser.add_argument("-o","--output_npz", type=str, help="the path to save the embeddings to. Saved in NPZ format")
+    parser.add_argument("-d", "--debug", action='store_true', help="run with a small dataset", default=False)
+    parser.add_argument("-p", "--pred-only", action='store_true', default=False, help="only save predictions, otherwise save original image data and labels in output_npz")
+
+    args = parser.parse_args(argv)
+
+    logger = get_logger('info')
+    transform = get_transform()  
+
+    predict_files = args.prediction
+
+    logger.info(f"Loading predicting data: {len(predict_files)} files")
+    predict_dataset = LMDataset(predict_files, transform=transform, logger=logger, return_labels=True, label_types=args.labels)
+
+    for i in range(predict_dataset.labels.shape[1]):
+        logger.info(predict_dataset.label_types[i] + " - " + str(torch.unique(predict_dataset.labels[:, i])) + str(predict_dataset.label_classes))
+
+    true_labels = predict_dataset.labels[:, 0]  ##
+
+    predict_loader = DataLoader(predict_dataset, batch_size=32, shuffle=False, drop_last=False, num_workers=3)
+
+    model = ResNet.load_from_checkpoint(args.checkpoint)
+    accelerator = "gpu" if torch.cuda.is_available() else "cpu"
+    trainer = L.Trainer(devices=1, accelerator=accelerator)
+
+    logger.info("Running predictions")
+    predictions = trainer.predict(model, predict_loader)
+    predictions = torch.cat(predictions).numpy()
+
+    pred_labels = np.argmax(predictions, axis=1)  ##
+
+    if true_labels is not None: ##
+        accuracy = accuracy_score(true_labels, pred_labels)
+        precision = precision_score(true_labels, pred_labels, average='weighted')
+        recall = recall_score(true_labels, pred_labels, average='weighted')
+        conf_matrix = confusion_matrix(true_labels, pred_labels)
+
+        logger.info(f"Accuracy: {accuracy:.4f}")
+        logger.info(f"Precision: {precision:.4f}")
+        logger.info(f"Recall: {recall:.4f}")
+        logger.info(f"Confusion Matrix:\n{conf_matrix}")
+
+    # out_data = dict(predictions=predictions)
+    out_data = dict(predictions=predictions, true_labels=true_labels)  ##Recently added
+
+
+    if not args.pred_only:
+        dset = predict_dataset
+        out_data['images'] = np.asarray(torch.squeeze(dset.data))
+        for i, k in enumerate(dset.label_types):
+            out_data[k + "_classes"] = dset.label_classes[i]
+            out_data[k + "_labels"] = np.asarray(dset.labels[:, i])
+
+    np.savez(args.output_npz, **out_data)
 
 if __name__ == '__main__':
     train()
