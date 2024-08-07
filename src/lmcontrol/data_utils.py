a6d1c9dea115e1b64fc34d1f63b6c10c0fec1b47
NiranjanChaudhari0929 <f20200929@pilani.bits-pilani.ac.in>
2024-08-07 13:29:15 -0700

    Mofified for data size functionality

--- src/lmcontrol/data_utils.py
+++ src/lmcontrol/data_utils.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
@@ -6,8 +8,9 @@ from .utils import get_logger
 def write_npz(path, images, masks, paths, **metadata):
     np.savez(path, masks=masks, images=images, paths=paths, **metadata)
 
+# make label_types a string (Andrew)
 
-def load_npzs(npzs, logger):
+def load_npzs(npzs, logger, n=None, label_types=None):
     """Load data from NPZ files generated from lmcontrol crop command"""
     masks = list()
     images = list()
@@ -37,6 +40,50 @@ def load_npzs(npzs, logger):
     images = np.concatenate(images)
     logger.debug("Merging paths")
     paths = np.concatenate(paths)
+    #logger.info(f"Metadata keys: {metadata.keys()}")
+    # label_types_array = np.array(label_types)
+   
+    if n is not None:
+        metadata = {k: np.array(v) for k, v in metadata.items()}
+
+        # # class_label_key = 'time' 
+        # class_label_key = label_types_array
+        # #classes = np.unique(metadata[class_label_key])
+        # labels = np.array(metadata[class_label_key])
+        # classes = np.unique(labels)
+        
+        
+
+        selected_idx = []   \
+        
+        #UNCOMMENTED FOR EXPERIMENTATION
+        # for label_key in label_types:
+        #     labels = metadata[label_types]
+        #     if not isinstance(labels, np.ndarray):
+        #         labels = np.array(labels)
+        #     classes = np.unique(labels)
+        
+        labels = metadata[label_types]
+        classes = np.unique(labels)
+
+        for cls in classes:
+            class_indices = np.where(labels == cls)[0]
+
+            if len(class_indices) > n:
+                class_indices = np.random.permutation(class_indices)[:n]
+            selected_idx.extend(class_indices)
+
+        selected_idx = np.array(selected_idx)
+
+        selected_idx = np.random.permutation(selected_idx)
+
+        masks = masks[selected_idx]
+        images = images[selected_idx]
+        paths = paths[selected_idx]
+
+        for k in metadata.keys():
+            metadata[k] = metadata[k][selected_idx]
+
 
     target_len = len(masks)
     error = False
@@ -51,6 +98,7 @@ def load_npzs(npzs, logger):
     if error:
         raise ValueError("NPZ files do not have the same metadata keys. See CRITICAL messages in logs for more details")
 
+    
     return masks, images, paths, metadata
 
 def encode_labels(labels, return_classes=True):
@@ -60,4 +108,3 @@ def encode_labels(labels, return_classes=True):
         return enc.transform(labels), enc.classes_
     else:
         return enc.transform(labels)
-
