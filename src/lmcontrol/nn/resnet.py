28294b386d66ed6099a7c4f211b05f3f920f11ed
NiranjanChaudhari0929 <f20200929@pilani.bits-pilani.ac.in>
2024-09-12 13:14:32 -0700

    Added resnet.py to handle various models.

---
+++ src/lmcontrol/nn/resnet.py
@@ -0,0 +1,163 @@
+from functools import partial
+from typing import Any, Callable, List, Optional, Type, Union
+
+from torchvision.utils import _log_api_usage_once   # the relative imports have to be like these
+from torchvision.models.resnet import _resnet, Bottleneck, BasicBlock, conv1x1, conv3x3
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+torch.no_grad()
+
+# ***************** IPORTANT ************* #
+
+# # *************** The following model will expect input as :
+# input = torch.randn(32, 3, 32, 32)   # batch_size=32, 3 channels, 32x32 image
+
+# block = Bottleneck  #BasicBlock reduces the paramaters
+# layers = [0, 1, 0, 0]
+# planes = [16, 32, 64, 128]
+
+# my_resnet = ResNet(block, layers, planes=planes, num_classes=3)  #BasicBlock will only have the width_per_group=64 and groups=1 only. Use bottleneck otherwise 
+
+# # *************** To view the model:
+# my_resnet  # Shows the summary of the model
+
+# # *************** To check the results:
+# with torch.no_grad():
+#     ret = my_resnet(input)
+# ret.shape
+
+# num_params = sum(p.numel() for p in my_resnet.parameters() if p.requires_grad)
+# print(f"Total number of trainable parameters: {num_params}")
+
+class ResNet(nn.Module):
+    def __init__(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        layers: List[int],
+        planes: Optional[List[int]] = None,  ##newly added stuff, we basically need to reduce the paramaters in the code so that we deal with less parameters
+        num_classes: int = 1000, #this 1000 can be overwritten with the input you desire
+        zero_init_residual: bool = False,
+        groups: int = 1,
+        width_per_group: int = 64,
+        replace_stride_with_dilation: Optional[List[bool]] = None,
+        norm_layer: Optional[Callable[..., nn.Module]] = None,
+    ) -> None:
+        super().__init__()
+        _log_api_usage_once(self)
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = planes[0] # This was hardcooded to be 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError(
+                "replace_stride_with_dilation should be None "
+                f"or a 3-element tuple, got {replace_stride_with_dilation}"
+            )
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        if planes is None:
+            planes = [64, 128, 256, 512]
+        
+        self.layer1 = self._make_layer(block, planes[0], layers[0])
+        self.layer2 = self._make_layer(block, planes[1], layers[1], stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, planes[2], layers[2], stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, planes[3], layers[3], stride=2, dilate=replace_stride_with_dilation[2])
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        idx = max(i for i in range(len(layers)) if layers[i] != 0) #**********added 
+        self.fc = nn.Linear(planes[idx] * block.expansion, num_classes)
+
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck) and m.bn3.weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock) and m.bn2.weight is not None:
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+
+    def _make_layer(
+        self,
+        block: Type[Union[BasicBlock, Bottleneck]],
+        planes: int,
+        blocks: int,
+        stride: int = 1,
+        dilate: bool = False,
+    ) -> nn.Sequential:
+        if blocks == 0:  # *******************this is added 
+            return nn.Identity()
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes, planes, stride, downsample, self.groups, self.base_width, previous_dilation, norm_layer
+            )
+        )
+        self.inplanes = planes * block.expansion  # *** Doing this isn't ethical in programming, NEVER change 'self' 
+        for _ in range(1, blocks):
+            layers.append(
+                block(
+                    self.inplanes,
+                    planes,
+                    groups=self.groups,
+                    base_width=self.base_width,
+                    dilation=self.dilation,
+                    norm_layer=norm_layer,
+                )
+            )
+
+        return nn.Sequential(*layers)
+
+    def _forward_impl(self, x: Tensor) -> Tensor:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self._forward_impl(x)
