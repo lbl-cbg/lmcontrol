2ab281f9085ecefaaf61e15bf2e99deafcc44cd2
NiranjanChaudhari0929 <136962491+NiranjanChaudhari0929@users.noreply.github.com>
2024-10-25 12:12:25 -0700

    Update scripts/byol_predict_simple.sh
    Co-authored-by: Andrew Tritt <ajtritt@lbl.gov>

--- scripts/byol_predict_simple.sh
+++ scripts/byol_predict_simple.sh
@@ -1,5 +1,8 @@
+INPUT_DIR=${1:?"Please provide the input data directory"};
+OUTPUT_DIR=${2:?"Please provide the directory to save results to"};
+CKPT=${3:?"Please provide the checkpoint to use for inference"};
 lmcontrol infer-byol \
-    -c /pscratch/sd/n/niranjan/output/checkpoint/epoch=9-step=15550.ckpt \
+    -c $CKPT \
     --prediction \
     /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S10/S10_HT3/all_processed.npz \
     /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S14/S14_HT6/all_processed.npz \
