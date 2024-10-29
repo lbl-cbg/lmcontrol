cf3fd071ce4377a2f922186c769e0a2e73bc6cf3
NiranjanChaudhari0929 <f20200929@pilani.bits-pilani.ac.in>
2024-10-29 15:01:58 -0700

    metadata forwarding issue solved

--- src/lmcontrol/nn/clf.py
+++ src/lmcontrol/nn/clf.py
@@ -450,6 +450,10 @@ def predict(argv=None):
     if not args.pred_only:
         dset = predict_dataset
         out_data['images'] = np.asarray(torch.squeeze(dset.data))
+        out_data['ht'] = np.asarray(dset.metadata['ht'])
+        out_data['time'] = np.asarray(dset.metadata['time'])
+        out_data['feed'] = np.asarray(dset.metadata['feed'])
+        out_data['sample'] = np.asarray(dset.metadata['sample'])
         for i, k in enumerate(dset.label_type):
             out_data[k + "_labels"] = np.asarray(dset.labels[:, i])
 
