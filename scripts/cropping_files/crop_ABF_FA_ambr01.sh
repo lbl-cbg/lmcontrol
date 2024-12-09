ae8055231b6ea54ccb8aebd8f9c10a6fa4cb1260
NiranjanChaudhari0929 <f20200929@pilani.bits-pilani.ac.in>
2024-12-09 15:26:25 -0800

    corrected few commands in cropping files.

--- scripts/cropping_files/crop_ABF_FA_ambr01.sh
+++ scripts/cropping_files/crop_ABF_FA_ambr01.sh
@@ -1,14 +1,14 @@
 # This list of commands is only for S1.
 # You may have to change these for different SX (where X are integers), and SX mean ssamples
 
-lmcontrol crop -n -u -c 96,96 -m "ht=HT2,sample=S1,conditions=Control" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT2 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT2/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT4,sample=S1,conditions=Control" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT4 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT4/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT5,sample=S1,conditions=Control" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT5 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT5/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT7,sample=S1,conditions=Temperature Shift (33°C) after 48h" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT7 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT7/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT8,sample=S1,conditions=Temperature Shift (33°C) after 48h" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT8 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT8/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT9,sample=S1,conditions=Temperature Shift (33°C) after 48h" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT9 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT9/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT10,sample=S1,conditions=DO saturation (15%)" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT10 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT10/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT11,sample=S1,conditions=DO saturation (15%)" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT11 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT11/
-lmcontrol crop -n -u -c 96,96 -m "ht=HT12,sample=S1,conditions=DO saturation (15%)" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT12 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT12/
+lmcontrol crop -n -u -c 96,96 -m "ht=2,sample=S1,condition=Control" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT2 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT2/
+lmcontrol crop -n -u -c 96,96 -m "ht=4,sample=S1,condition=Control" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT4 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT4/
+lmcontrol crop -n -u -c 96,96 -m "ht=5,sample=S1,condition=Control" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT5 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT5/
+lmcontrol crop -n -u -c 96,96 -m "ht=7,sample=S1,condition=Temperature Shift (33°C) after 48h" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT7 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT7/
+lmcontrol crop -n -u -c 96,96 -m "ht=8,sample=S1,condition=Temperature Shift (33°C) after 48h" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT8 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT8/
+lmcontrol crop -n -u -c 96,96 -m "ht=9,sample=S1,condition=Temperature Shift (33°C) after 48h" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT9 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT9/
+lmcontrol crop -n -u -c 96,96 -m "ht=10,sample=S1,condition=DO saturation (15%)" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT10 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT10/
+lmcontrol crop -n -u -c 96,96 -m "ht=11,sample=S1,condition=DO saturation (15%)" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT11 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT11/
+lmcontrol crop -n -u -c 96,96 -m "ht=12,sample=S1,condition=DO saturation (15%)" /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT12 /pscratch/sd/n/niranjan/tar_ball/ABF_FA_AMBR01-NSR/S1/S1_HT12/
 
 
--- scripts/cropping_files/crop_ambr01.sh
+++ scripts/cropping_files/crop_ambr01.sh
@@ -1,6 +1,6 @@
 # This is a template to use lmcontrol crop command on ambr01 data
 
-lmcontrol crop -n -u -c 96,96 -m "ht=HT1,time=28.0,sample=S1,feed=Glucose Only" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT1 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT1
-lmcontrol crop -n -u -c 96,96 -m "ht=HT4,time=28.0,sample=S1,feed=C:N 6" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT4 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT4
-lmcontrol crop -n -u -c 96,96 -m "ht=HT7,time=28.0,sample=S1,feed=C:N 15" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT7 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT7
-lmcontrol crop -n -u -c 96,96 -m "ht=HT10,time=28.0,sample=S1,feed=C:N 30" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT10 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT10
+lmcontrol crop -n -u -c 96,96 -m "ht=1,time=28.0,sample=S1,feed=Glucose Only" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT1 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT1
+lmcontrol crop -n -u -c 96,96 -m "ht=4,time=28.0,sample=S1,feed=C:N 6" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT4 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT4
+lmcontrol crop -n -u -c 96,96 -m "ht=7,time=28.0,sample=S1,feed=C:N 15" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT7 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT7
+lmcontrol crop -n -u -c 96,96 -m "ht=10,time=28.0,sample=S1,feed=C:N 30" /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT10 /pscratch/sd/n/niranjan/tar_ball/segmented_square_96/S4/S4_HT10
