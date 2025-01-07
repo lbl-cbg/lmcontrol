11ed324c416afd022092783c5082f8aa13a26c39
ajtritt <ajtritt@lbl.gov>
2025-01-07 15:35:27 -0800

    save stuff

--- src/lmcontrol/nn/byol.py
+++ src/lmcontrol/nn/byol.py
@@ -209,8 +209,17 @@ def predict(argv=None):
     parser.add_argument("checkpoint", type=str, help="path to the model checkpoint file to use for inference")
     parser.add_argument("prediction", type=str, nargs='+', required=True, help="directories containing prediction data")
     parser.add_argument("-d", "--debug", action='store_true', help="run with a small dataset", default=False)
+<<<<<<< Updated upstream
     parser.add_argument("-p", "--pred-only", action='store_true', default=False, help="only save predictions, otherwise save original image data and labels in output_npz")
     parser.add_argument("-n", "--n_samples", type=int, help="number of samples to use from each class", default=None)
+=======
+    parser.add_argument("-p", "--pred-only", action='store_true', default=False, help="only save predictions, otherwise save original image data and labels in output")
+    parser.add_argument("-n", "--n-samples", type=int, help="number of samples to use from each class", default=None)
+    parser.add_argument("-b", "--batch-size", type=int, help="batch size for training and validation", default=64)
+    parser.add_argument("-g", "--devices", type=int, help="number of devices for trainer", default=1)
+    parser.add_argument("-V", "--add-viz", action='store_true', help="Compute UMAP embedding for visualiztion", default=False)
+    parser.add_argument("-2", "--two-dim", action='store_true', help="Compute 2D UMAP embedding (default is 3D embedding)", default=False)
+>>>>>>> Stashed changes
 
     args = parser.parse_args(argv)
 
@@ -220,7 +229,17 @@ def predict(argv=None):
     logger.info(f"Loading inference data: {len(args.prediction)} files")
     test_dataset = LMDataset(args.prediction, transform=transform, logger=logger, return_labels=False, n_samples=args.n_samples)
 
+<<<<<<< Updated upstream
     test_dl = DataLoader(test_dataset, batch_size=512, shuffle=False, drop_last=False, num_workers=3)
+=======
+    loader = get_loaders(args,
+                         inference=True,
+                         tfm=transform,
+                         return_labels=False,
+                         logger=logger)
+    dataset = loader.dataset
+
+>>>>>>> Stashed changes
 
     model = BYOL.load_from_checkpoint(args.checkpoint)
     accelerator = "gpu" if torch.cuda.is_available() else "cpu"
@@ -239,7 +258,28 @@ def predict(argv=None):
 
     logger.info("Saving output")
 
+<<<<<<< Updated upstream
     np.savez(args.output_npz, **out_data)
+=======
+
+
+    if args.add_viz:
+        if accelerator == "gpu":
+            from cuml import UMAP
+        else:
+            from umap import UMAP
+        D = 2 if args.two_dim else 3
+        umap = UMAP(n_components=D,
+                    min_dist=0.1, metric='euclidean')
+        logger.info(f"Calculating {D}D embedding for visualization")
+        emb = umap.fit_transform(predictions)
+        t.add_viz_embedding(emb,
+                            description=f"UMAP embedding computed using {str(umap.get_params())}")
+
+
+    with get_hdf5io(args.output, mode='w') as io:
+        io.write(t)
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
